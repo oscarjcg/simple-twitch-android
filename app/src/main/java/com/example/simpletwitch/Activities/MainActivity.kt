a898f2ab69d3eec42f50cd5a9b200fa690f48e8e
Oscar Catari Gutierrez <oscarcatari@outlook.es>
2021-10-04 21:51:03 +0100

    [ADD] Navigation graph

--- app/src/main/java/com/example/simpletwitch/Activities/MainActivity.kt
+++ app/src/main/java/com/example/simpletwitch/Activities/MainActivity.kt
@@ -2,19 +2,13 @@ package com.example.simpletwitch.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Window
-import androidx.viewpager2.widget.ViewPager2
-import com.example.simpletwitch.Adapters.NavAdapter
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.simpletwitch.R
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var viewPager2: ViewPager2
-    private lateinit var navBar: TabLayout
-
-    private  lateinit var navAdapter: NavAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,26 +16,9 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.hide()
         setContentView(R.layout.activity_main)
 
-        // Bind view
-        initView()
-
-        // Init navigation with viewpager2
-        navAdapter = NavAdapter(this)
-        viewPager2.adapter = navAdapter
-        TabLayoutMediator(navBar, viewPager2) { tab, position ->
-            var name = getString(R.string.following_tab);
-            when(position) {
-                NavAdapter.FOLLOWING_POS -> name = getString(R.string.following_tab)
-                NavAdapter.DISCOVER_POS -> name = getString(R.string.discover_tab)
-                NavAdapter.BROWSE_POS -> name = getString(R.string.browse_tab)
-            }
-            tab.text= name
-        }.attach()
-
-    }
-
-    fun initView() {
-        viewPager2 = findViewById(R.id.navViewPager2)
-        navBar = findViewById(R.id.navBar)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController: NavController = navHostFragment.navController
+        navBottom.setupWithNavController(navController)
     }
 }
--- app/src/main/java/com/example/simpletwitch/Fragments/BrowseFragment.kt
+++ app/src/main/java/com/example/simpletwitch/Fragments/BrowseFragment.kt
@@ -5,13 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.FrameLayout
-import androidx.viewpager2.widget.ViewPager2
 import com.example.simpletwitch.Activities.MainActivity
 import com.example.simpletwitch.Adapters.BrowseNavAdapter
-import com.example.simpletwitch.Adapters.NavAdapter
 import com.example.simpletwitch.R
-import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.fragment_browse.*
 
